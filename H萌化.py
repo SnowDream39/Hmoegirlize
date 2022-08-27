f9f659d119a598d4b9f4be09292d4fccb1e065ec
SnowDream39 <108746050+SnowDream39@users.noreply.github.com>
2022-08-27 23:21:21 +0800

    Add files via upload

---
+++ H萌化.py
@@ -0,0 +1,23 @@
+original=open('原文.txt','r',encoding='UTF-8')
+after=open('处理后.txt','w', encoding='UTF-8')
+after.truncate()
+length=len(original.readlines())
+original.seek(0)
+for p in range(length):
+    o=original.readline()
+    i=0
+    while i < len(o):
+        if o[i:i+2]=='[[':
+            after.write('{{萌百|')
+            i+=2
+            while o[i:i+2]!=']]':
+                after.write(o[i])
+                i+=1
+            else:
+                after.write('}}')
+                i+=2
+        else:
+            after.write(o[i])
+            i+=1
+original.close()
+after.close()
